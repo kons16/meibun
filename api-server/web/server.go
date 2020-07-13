cbab5eb9bd5937dd1d641489b0cebf5029af06b1
kons16 <ks1216r@gmail.com>
2020-07-13 21:53:23 +0900

    web/serverのhandler()の実装

--- api-server/web/server.go
+++ api-server/web/server.go
@@ -1,1 +1,66 @@
 package web
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const sessionKey = ""
+
+type Server interface {
+	Handler() *echo.Echo
+}
+
+type server struct {
+}
+
+func NewServer() {
+}
+
+func (s *server) Handler() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.SecureWithConfig(middleware.SecureConfig{
+		XSSProtection:         "1; mode=block",
+		ContentTypeNosniff:    "nosniff",
+		XFrameOptions:         "DENY",
+		HSTSMaxAge:            3600,
+		ContentSecurityPolicy: "default-src 'self'",
+	}))
+
+	e.GET("/", s.indexHandler)
+	e.GET("/signup", s.willSignupHandler)
+	e.POST("/signup", s.signupHandler)
+	e.GET("/signin", s.willSigninHandler)
+	e.POST("/signin", s.signinHandler)
+	e.POST("/signout", s.signoutHandler)
+
+	return e
+}
+
+
+func (s *server) indexHandler(c echo.Context) error {
+	return nil
+}
+
+func (s *server) willSignupHandler(c echo.Context) error {
+	return nil
+}
+
+func (s *server) signupHandler(c echo.Context) error {
+	return nil
+}
+
+func (s *server) signoutHandler(c echo.Context) error {
+	return nil
+}
+
+func (s *server) willSigninHandler(c echo.Context) error {
+	return nil
+}
+
+func (s *server) signinHandler(c echo.Context) error {
+	return nil
+}
