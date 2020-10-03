0c49dd9ad8654d3e61a51e9d716d97d148029562
kons16 <ks1216r@gmail.com>
2020-10-03 21:47:14 +0900

    Test_WillSignupHandlerの追加

--- api-server/web/auth_test.go
+++ api-server/web/auth_test.go
@@ -12,6 +12,30 @@ import (
 	"testing"
 )
 
+// GET /signup
+func Test_WillSignupHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockService := mock_service.NewMockMeibunApp(ctrl)
+	var user model.User
+	user.ID = 1
+	mockService.EXPECT().FindUserByToken("token").Return(&user, nil)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/signup", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	req.AddCookie(&http.Cookie{Name: sessionKey, Value: "token"})
+	c := e.NewContext(req, rec)
+	// c.SetCookie(&http.Cookie{Name: sessionKey, Value: "token"})
+
+	app := &server{app: mockService}
+	_ = app.willSignupHandler(c)
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+}
+
 // POST /signup
 func Test_SignupHandler(t *testing.T) {
 	ctrl := gomock.NewController(t)
